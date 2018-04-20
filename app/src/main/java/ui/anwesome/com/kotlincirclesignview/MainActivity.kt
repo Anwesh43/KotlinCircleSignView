01cca4fa2e0c06d7ad8ad27020e68f163ae3aaa0
Anwesh43 <anweshthecool0@gmail.com>
2018-04-20 09:45:33 +0530

    using circlesignview's OnCompleteListener in activity

--- app/src/main/java/ui/anwesome/com/kotlincirclesignview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlincirclesignview/MainActivity.kt
@@ -3,13 +3,19 @@ package ui.anwesome.com.kotlincirclesignview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import ui.anwesome.com.circlesignview.CircleSignView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        CircleSignView.create(this)
+        var view : CircleSignView = CircleSignView.create(this)
+        view.addCompleteListener({
+            Toast.makeText(this, "finished", Toast.LENGTH_SHORT).show()
+        },{
+            Toast.makeText(this, "restarted", Toast.LENGTH_SHORT).show()
+        })
         fullScreen()
     }
 }
