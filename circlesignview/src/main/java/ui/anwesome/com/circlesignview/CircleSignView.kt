bbc11254144f6d3386e4b9eddf81d8f2b3d6e41f
Anwesh43 <anweshthecool0@gmail.com>
2018-04-20 09:27:23 +0530

    adding fixes in circlesignview

--- circlesignview/src/main/java/ui/anwesome/com/circlesignview/CircleSignView.kt
+++ circlesignview/src/main/java/ui/anwesome/com/circlesignview/CircleSignView.kt
@@ -30,7 +30,7 @@ class CircleSignView(ctx : Context) : View(ctx) {
 
     data class State (private var prevScale : Float = 0f, var dir : Float = 0f, var j : Int = 0) {
 
-        val scales : Array<Float> = arrayOf(0f, 0f, 0f)
+        val scales : Array<Float> = arrayOf(0f, 0f, 0f, 0f, 0f)
 
         fun update(stopcb : (Float) -> Unit) {
             scales[j] += 0.1f * this.dir
@@ -41,6 +41,7 @@ class CircleSignView(ctx : Context) : View(ctx) {
                     j -= dir.toInt()
                     dir = 0f
                     prevScale = scales[j]
+                    stopcb(prevScale)
                 }
             }
         }
@@ -85,30 +86,31 @@ class CircleSignView(ctx : Context) : View(ctx) {
     data class CircleSign (var i : Int , private val state : State = State()) {
 
         fun draw(canvas : Canvas, paint : Paint) {
-            val w : Float = canvas.width.toFloat()
-            val h : Float = canvas.height.toFloat()
-            val r1 : Float = Math.min(w,h)/3
-            val r2 : Float = Math.min(w,h)/20
-            canvas.save()
-            canvas.translate(w/2, h/2)
-            paint.style = Paint.Style.STROKE
-            paint.strokeWidth = Math.min(w, h)/60
-            paint.strokeCap = Paint.Cap.ROUND
-            paint.color = Color.parseColor("#e74c3c")
-            for (i in 0..1) {
-                canvas.drawArc(RectF(-r1, -r1, r1, r1), 180f * i, 180f * state.scales[0], false, paint)
+            if (state.scales.size == 5) {
+                val w: Float = canvas.width.toFloat()
+                val h: Float = canvas.height.toFloat()
+                val r1: Float = Math.min(w, h) / 3
+                val r2: Float = Math.min(w, h) / 20
+                canvas.save()
+                canvas.translate(w / 2, h / 2)
+                paint.style = Paint.Style.STROKE
+                paint.strokeWidth = Math.min(w, h) / 60
+                paint.strokeCap = Paint.Cap.ROUND
+                paint.color = Color.parseColor("#2ecc71")
+                for (i in 0..1) {
+                    canvas.drawArc(RectF(-r1, -r1, r1, r1), 180f * i, 180f * state.scales[0], false, paint)
+                }
+                canvas.save()
+                canvas.rotate(180f * this.state.scales[4])
+                paint.style = Paint.Style.FILL
+                for (i in 0..1) {
+                    canvas.drawCircle(0f, (r1 / 4) * (1 - 2 * i) * state.scales[2], r2 * this.state.scales[1], paint)
+                }
+                val x: Float = (r1 / 5) * state.scales[3]
+                canvas.drawLine(-x, 0f, x, 0f, paint)
+                canvas.restore()
+                canvas.restore()
             }
-            canvas.save()
-            canvas.rotate(180f * this.state.scales[4])
-            paint.style = Paint.Style.FILL
-            paint.color = Color.parseColor("#2ecc71")
-            for(i in 0..1) {
-                canvas.drawCircle(0f, (r1/4) * (1 - 2 * i) * state.scales[2], r2 * this.state.scales[1], paint)
-            }
-            val x : Float = (r1/5) * state.scales[3]
-            canvas.drawLine(-x, 0f, x, 0f, paint)
-            canvas.restore()
-            canvas.restore()
         }
 
         fun update(stopcb : (Float) -> Unit) {
