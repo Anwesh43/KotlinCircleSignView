6e734d55fcf70d7ff74bbde20ef35356d18fc88a
Anwesh43 <anweshthecool0@gmail.com>
2018-04-20 09:14:27 +0530

    adding circlesignview in activity

--- app/src/main/java/ui/anwesome/com/kotlincirclesignview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlincirclesignview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.kotlincirclesignview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.circlesignview.CircleSignView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        CircleSignView.create(this)
     }
 }
