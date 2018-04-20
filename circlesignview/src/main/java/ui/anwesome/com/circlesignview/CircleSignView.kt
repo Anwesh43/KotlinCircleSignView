8eafac28a0dd238ff1d980b9dd475ee497b0f9cd
Anwesh43 <anweshthecool0@gmail.com>
2018-04-20 08:19:19 +0530

    adding CircleSignView

--- circlesignview/src/main/java/ui/anwesome/com/circlesignview/CircleSignView.kt
+++ circlesignview/src/main/java/ui/anwesome/com/circlesignview/CircleSignView.kt
@@ -3,3 +3,25 @@ package ui.anwesome.com.circlesignview
 /**
  * Created by anweshmishra on 20/04/18.
  */
+
+import android.view.*
+import android.content.*
+import android.graphics.*
+
+class CircleSignView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
